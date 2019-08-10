b4bd45ea4cb0d524edc4b7e8db875d878b891458
wcrozier12 <wccrozier12@gmail.com>
2019-08-10 14:30:21 -0700

    initial commit, added grid and images

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img className="item-1" sizes="(min-width: 769px) 25vw, (min-width: 376px) 50vw, 100vw" srcSet="https://dummyimage.com/400 400w, https://dummyimage.com/786 786w, https://dummyimage.com/900 900w, https://dummyimage.com/1080 1080w, https://dummyimage.com/2400 2400w" />
+      <img className="item-2" srcSet="https://dummyimage.com/400 400w, https://dummyimage.com/786 786w, https://dummyimage.com/900 900w, https://dummyimage.com/1080 1080w, https://dummyimage.com/2400 2400w" />
+      <img className="item-3" srcSet="https://dummyimage.com/400 400w, https://dummyimage.com/786 786w, https://dummyimage.com/900 900w, https://dummyimage.com/1080 1080w, https://dummyimage.com/2400 2400w" />
+      <img className="item-4" srcSet="https://dummyimage.com/400 400w, https://dummyimage.com/786 786w, https://dummyimage.com/900 900w, https://dummyimage.com/1080 1080w, https://dummyimage.com/2400 2400w" />
     </div>
   );
 }
